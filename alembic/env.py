35ceaabe43987b291b41e17901f0294a6c493379
nada <nadaamin00@gmail.com>
2026-06-27 22:50:01 +0300

    Fix url issue

--- alembic/env.py
+++ alembic/env.py
@@ -62,6 +62,8 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    config.set_main_option("sqlalchemy.url", settings.database_url)
+
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
